1afa5d8a7f5e0f4b2360919227180978c0c7eb8b
Matt White <mwhite@doximity.com>
2020-12-10 11:54:29 -0800

    Part 2 solution

--- day10/solution.py
+++ day10/solution.py
@@ -1,20 +1,26 @@
 import pathlib
 
-with open(pathlib.Path(__file__).parent / 'input.txt') as file:
-    adapters = file.read().splitlines()
-    adapters = list(map(int, adapters))
-    adapters.append(0)
-    adapters.sort()
-    adapters.append(adapters[-1] + 3)
 
-one_count = 0
-three_count = 1
+with open(pathlib.Path(__file__).parent / 'input.txt') as file:
+    adapters = sorted([int(x) for x in file])
+
+# Add in outlet joltage of 0 and my device of max_adapter + 3
+adapters.insert(0, 0)
+adapters.append(adapters[-1] + 3)
+counts = {1: 0, 3: 0}
+
 for index in range(0, len(adapters) - 1):
     diff = adapters[index + 1] - adapters[index]
-    if diff == 1:
-        one_count += 1
-    elif diff == 3:
-        three_count += 1
+    counts[diff] += 1
 
-print(one_count * three_count)
+print(counts[1] * counts[3])
 # 2048
+
+adapters.pop(0)
+options = {}
+options[0] = 1
+for a in adapters:
+    options[a] = options.get(a - 1, 0) + options.get(a - 2, 0) + options.get(a - 3, 0)
+
+print(options[max(adapters)])
+# 1322306994176
